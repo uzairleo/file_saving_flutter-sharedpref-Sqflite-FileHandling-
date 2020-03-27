c03b571d3dc7b2b0bd5d4db4fa7bb764bcc5e973
Uzair Leo <uzair.jan336@gmail.com>
2020-03-27 18:52:50 +0500

    separate all files

--- lib/DatabaseHelper.dart
+++ lib/DatabaseHelper.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sqflite/sqflite.dart';
-
+//adding a databaseHelper File and then one for shared Prefernces
 class DatabaseHelper{
 
-  
+
 }
