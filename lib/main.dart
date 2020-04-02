852420101ed9453ea2d38f593ce3aa446b8de135
Uzair Leo <uzair.jan336@gmail.com>
2020-04-02 15:17:35 +0500

    connect splashScreen with main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:saving_file_locally/FileHandling.dart';
 import 'package:saving_file_locally/sharedPreferences.dart';
 import 'package:saving_file_locally/DatabaseHelper.dart';
+import 'package:saving_file_locally/splashscreen.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -14,7 +15,8 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),//Three ways to store data locally(serverless methods)
       home:
-       MyApps(),//_SharedPreferences_Demonstration
+      SplashScreen(),
+      //  MyApps(),//_SharedPreferences_Demonstration
       // MyHomePage(title: 'Flutter Demo Home Page'),//_File_Handling_Demonstration
       //  DatabaseOne(),//_Database_Demonstration
     );
